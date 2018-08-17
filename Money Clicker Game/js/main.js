f3833f7180c92461b182cd0addfa25d5bd7e238a
tiwarir1 <rabi.tiwari5788@gmail.com>
2018-08-17 16:35:02 -0500

    Feat and refactor

--- Money Clicker Game/js/main.js
+++ Money Clicker Game/js/main.js
@@ -1,6 +1,15 @@
 var moneyClicks = 0;
 var workers = 0;
 var workerCost = 10;
+var workersMPS = 1;
+
+var teamLeader = 0;
+var teamLeaderCost = 25;
+var teamLeaderMPS = 2;
+
+var manager = 0;
+var managerCost = 65;
+var managerMPS = 5;
 
 
 function moneyClicked() {
@@ -15,28 +24,81 @@ var moneyClickerTime = setInterval(function() {
 
 function updateMoney() {
 	var moneyPerSecond = 0;
-	moneyPerSecond = workers;
+	moneyPerSecond = workers + (teamLeader*teamLeaderMPS) + (manager * managerMPS);
 	document.getElementById("workerCost").innerHTML = workerCost + "$";
 	document.getElementById("moneyPerSec").innerHTML = moneyPerSecond + "$/sec";
 	document.getElementById("moneyAmount").innerHTML = moneyClicks + "$";
+	document.getElementById("numberOfWorkers").innerHTML = workers + " Workers";
+
+	document.getElementById("teamLeaderCost").innerHTML = teamLeaderCost + "$";
+	document.getElementById("numberOfTeamLeader").innerHTML = teamLeader + " Team Leaders"
+
+	document.getElementById("managerCost").innerHTML = managerCost + "$";
+	document.getElementById("numberOfManager").innerHTML = manager + " Managers";
 }
 
+
+
+function getStaff(items, cost) {
+	if (moneyClicks >= window[cost]) {
+		window[items]++;
+		moneyClicks -= window[cost];
+		window[cost] = Math.round(window[cost] * 1.15);
+	}
+	else {
+		alert('not enough money');
+	}
+}
+/*
 function getWorker() {
 	if (moneyClicks >= workerCost){
 		workers++;
 		moneyClicks -= workerCost;
-		workerCost = Math.round(workerCost *1.5);
+		workerCost = Math.round(workerCost *1.15);
 	}
 	else {
 		alert("not enough money");
 	}
 }
 
+function getTeamLeader() {
+	if (moneyClicks >= teamLeaderCost) {
+		moneyClicks -= teamLeaderCost;
+		teamLeader++;
+		teamLeaderCost = Math.round(teamLeaderCost * 1.15);
+	}
+	else {
+		alert("not enough money");
+	}
+}
+
+function getManager() {
+	if (moneyClicks >= managerCost) {
+		moneyClicks -= managerCost;
+		manager++;
+		managerCost = Math.round(managerCost * 1.15);
+	}
+	else {
+		alert("not enough money");
+	}
+}
+
+*/
+
 var moneyProducerTimer = setInterval(function() {
 	moneyProducers();
 }, 1000);
 
+
+//The amount of money produced per second is generated here
 function moneyProducers() {
 	moneyClicks += workers;
+	moneyClicks += teamLeader * teamLeaderMPS;
+	moneyClicks += manager * managerMPS;
 
+}
+
+function popup() {
+	//document.getElementById("popup-info").classList.add("visible");
+	document.getElementById("popup-info").classList.toggle("visible");
 }
